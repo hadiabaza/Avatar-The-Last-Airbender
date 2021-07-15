25a5fa67b0c539c14f8ea866f0610cc500b3398e
Hadi Abaza <Hadiabaza1@gmail.com>
2021-07-15 16:13:01 -0400

    about component

--- src/About.js
+++ src/About.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h5>HELLO!</h5>
-            <p>We created a wonderful page that allows you to admire your favorite characters from the shows
-                "Avatar, The Last Airbender" and "The Legend of Korra." Click on the home button whenever you're ready.
-            </p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h5>HELLO!</h5>
+      <p id="about">
+        We created a wonderful page that allows you to admire your favorite
+        characters from the shows "Avatar, The Last Airbender" and "The Legend
+        of Korra." Click on the home button whenever you're ready.
+      </p>
+      <br></br>
+      <br></br>
+      <p id="about">
+        Revisit the Journey that follows 12-year-old Aang along with his friends
+        Katara and Sokka on there quest to defeat Fire Lord Ozai. <br></br>Along
+        there Journey, they made friends and foes, get yourself started by
+        checking out there cards!
+      </p>
+      <br></br>
+      <br></br>
+      <p id="about">
+        If you're up for it, check out the characters in the Legend of Korra!
+        Enjoy!
+      </p>
+    </div>
+  );
+};
 
 export default About;
