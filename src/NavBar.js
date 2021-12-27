99548199c736501dd968ff595cb3dfed38a4cbf8
Hadi Abaza <Hadiabaza1@gmail.com>
2021-12-26 23:25:19 -0500

    removed inacessible anchors

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React from "react";
 
-
-const NavBar = ({handleClick, setFilter, resetFilter}) => {
-    return (
-        <div id="Nav-Bar">
-            <div className="dropdown">
-                <button className="dropbtn">Filter</button>
-                <div className="dropdown-content">
-                    <a onClick={() => setFilter("affiliation")}>Affiliation</a>
-                    <a onClick={() => setFilter("element")}>Element</a>
-                    <a onClick={() => setFilter("show")}>Show</a>
-                    <a onClick={() => { 
-                        setFilter("all");
-                        resetFilter();
-                }}>All</a>
-                </div>
-            </div>
-            
-            <button
+const NavBar = ({ handleClick, setFilter, resetFilter }) => {
+  return (
+    <div id="Nav-Bar">
+      <div className="dropdown">
+        <button className="dropbtn">Filter</button>
+        <div className="dropdown-content">
+          <div onClick={() => setFilter("affiliation")}>Affiliation</div>
+          <div onClick={() => setFilter("element")}>Element</div>
+          <div onClick={() => setFilter("show")}>Show</div>
+          <div
             onClick={() => {
-            handleClick(false);
-            resetFilter();
+              setFilter("all");
+              resetFilter();
             }}
-            >Characters</button>
-            
-            <button
-            onClick={() => {
-            handleClick(true);
-            resetFilter();
-            }}
-            >Favorites</button>
-            
+          >
+            All
+          </div>
         </div>
-    );
-}
+      </div>
+
+      <button
+        onClick={() => {
+          handleClick(false);
+          resetFilter();
+        }}
+      >
+        Characters
+      </button>
+
+      <button
+        onClick={() => {
+          handleClick(true);
+          resetFilter();
+        }}
+      >
+        Favorites
+      </button>
+    </div>
+  );
+};
 
 export default NavBar;
